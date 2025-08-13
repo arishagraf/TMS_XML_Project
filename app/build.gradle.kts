523eb88ceb10a55bea00c521c33b5b2088c9be81
Aryna Sukhail <aryna.sukhail@proexe.pl>
2025-08-13 18:22:47 +0300

    urlConnection - mockAPI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     //gson to convert(serialize) api response to our kotlin data models
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //api logger
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     // viewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
